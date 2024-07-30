afe93ad968c4bec79a054093247fca645f6c96f2
ozen-hub <info.ozenhub@gmail.com>
2024-07-30 21:19:29 +0530

    init

--- src/main/java/com/devops/user_service_api_1/UserServiceApi1Application.java
+++ src/main/java/com/devops/user_service_api_1/UserServiceApi1Application.java
@@ -2,8 +2,10 @@ package com.devops.user_service_api_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApi1Application {
 
 	public static void main(String[] args) {
